d8c7ed9c05303467697e2bbc6f9da494e0fa4a2f
uyen.dev <vinhuyen.theonly@gmail.com>
2024-01-03 20:21:54 +0700

    init project \

---
+++ Test Suites/Test Suite Example.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Suite Example</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>7d8ed616-89c1-45ef-bfe4-8ebafe205e07</testSuiteGuid>
+</TestSuiteEntity>

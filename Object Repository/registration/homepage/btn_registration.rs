5a979fa1cfe8cae0fc031f59dcd5391cd033ff61
uyen.dev <vinhuyen.theonly@gmail.com>
2024-01-12 23:05:58 +0700

    User register

---
+++ Object Repository/registration/homepage/btn_registration.rs
@@ -0,0 +1,107 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>btn_registration</name>
+   <tag></tag>
+   <elementGuidId>6b724c5c-e10a-4070-9e2d-452b40be8e83</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='root']/header/div/nav/ul/li[2]/a</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>a</value>
+      <webElementGuid>a377d2cd-8a0b-4497-ab27-52e620abb6ec</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>title</name>
+      <type>Main</type>
+      <value>Đăng Ký</value>
+      <webElementGuid>8d266b02-467d-40ae-b15e-d89321f2c292</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value> border-r border-r-white/50 px-2 hover:opacity-80</value>
+      <webElementGuid>08e5a6e3-c20f-4117-843d-e4bad545f39a</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>href</name>
+      <type>Main</type>
+      <value>/register</value>
+      <webElementGuid>74df729b-9a58-49e7-89c2-015eccebcaf6</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Đăng Ký</value>
+      <webElementGuid>b73bca37-6b1d-483c-99fc-812b1bfc51e4</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;root&quot;)/header[@class=&quot;sticky inset-0 z-10&quot;]/div[@class=&quot;flex h-[2.125rem] items-center bg-orange&quot;]/nav[@class=&quot;container w-full&quot;]/ul[@class=&quot;flex items-center justify-end gap-4 text-[13px] text-white&quot;]/li[@class=&quot;flex items-center&quot;]/a[@class=&quot;border-r border-r-white/50 px-2 hover:opacity-80&quot;]</value>
+      <webElementGuid>42d24265-7274-4fbd-9a28-6615db544f78</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='root']/header/div/nav/ul/li[2]/a</value>
+      <webElementGuid>8fa23d52-05f5-4ef3-8c43-847f3c4361ce</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:link</name>
+      <type>Main</type>
+      <value>//a[contains(text(),'Đăng Ký')]</value>
+      <webElementGuid>b4ec9802-46a4-4798-a93f-cfb2f525d622</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:href</name>
+      <type>Main</type>
+      <value>//a[contains(@href, '/register')]</value>
+      <webElementGuid>f14b2cc7-93ec-4196-bf46-3b8636579698</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//a</value>
+      <webElementGuid>bb6b757b-91bf-477c-91c0-eb3460ee910d</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:customAttributes</name>
+      <type>Main</type>
+      <value>//a[@title = 'Đăng Ký' and @href = '/register' and (text() = 'Đăng Ký' or . = 'Đăng Ký')]</value>
+      <webElementGuid>3ca912ca-51b7-46be-a846-208fa5f3e0bc</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
